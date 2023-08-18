5127688b98d73263937384129be14ae749bf9d89
julzedz <edoziechukwuma@gmail.com>
2023-08-18 09:07:25 +0100

    Add 5 messages in message table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Message.create(content: 'Hi there!')
+Message.create(content: 'Welcome!')
+Message.create(content: 'Glad to see you again!')
+Message.create(content: 'Nice to see you!')
+Message.create(content: 'How you doing!')
